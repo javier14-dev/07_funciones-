75049a9756165308a4bb4373ea08ec99358fb315
javier14-dev <jcondorich@unjbg.edu.pe>
2025-06-02 17:38:41 -0500

    Agrega ejercicio 6: es numero perfecto

---
+++ ejercicio6.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+bool esPerfecto(int n) {
+    int suma = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (n % i == 0) {
+            suma += i;
+        }
+    }
+    return suma == n;
+}
+int main() {
+    int numero;
+    cout << "Ingrese un numero entero positivo: ";
+    cin >> numero;
+
+    if (numero <= 0) {
+        cout << "El numero debe ser positivo." << endl;
+        return 1;
+    }
+
+    if (esPerfecto(numero)) {
+        cout << numero << "Es un numero perfecto." << endl;
+    } else {
+        cout << numero << "No es un numero perfecto." << endl;
+    }
+    return 0;
+}
