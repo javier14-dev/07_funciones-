347b33a5f62442b4504c4be55e5cdab74411aa2e
javier14-dev <jcondorich@unjbg.edu.pe>
2025-06-03 17:52:57 -0500

    Actualiza ejercicio 1: agrega funcion resta

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -3,6 +3,9 @@ using namespace std;
 int suma(int a, int b) {
     return a + b;
 }
+int resta(int a, int b) {
+    return a - b;
+}
 float division(int a, int b) {
     if (b == 0) {
         cout << "Error: division entre cero no permitida." << endl;
@@ -19,9 +22,11 @@ int main() {
     cin >> num2;
 
     int resultadoSuma = suma(num1, num2);
+    int resultadoResta = resta(num1, num2);
     float resultadoDivision = division(num1, num2);
 
     cout << "Resultado de la suma: " << resultadoSuma << endl;
+    cout << "Resultado de la resta: " << resultadoResta << endl;
 
     if (num2 != 0) {
         cout << "Resultado de la division: " << resultadoDivision << endl;
