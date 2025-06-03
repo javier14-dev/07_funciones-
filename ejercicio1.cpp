ec6ebd282bb7cbd5ae3ae8e250dfa059730665a7
javier14-dev <jcondorich@unjbg.edu.pe>
2025-06-03 17:51:26 -0500

    Agrega ejercicio 1: funcion: suma y division

---
+++ ejercicio1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int suma(int a, int b) {
+    return a + b;
+}
+float division(int a, int b) {
+    if (b == 0) {
+        cout << "Error: division entre cero no permitida." << endl;
+        return 0.0;
+    }
+    return static_cast<float>(a) / b;
+}
+int main() {
+    int num1, num2;
+
+    cout << "Ingrese el primer numero entero: ";
+    cin >> num1;
+    cout << "Ingrese el segundo numero entero: ";
+    cin >> num2;
+
+    int resultadoSuma = suma(num1, num2);
+    float resultadoDivision = division(num1, num2);
+
+    cout << "Resultado de la suma: " << resultadoSuma << endl;
+
+    if (num2 != 0) {
+        cout << "Resultado de la division: " << resultadoDivision << endl;
+    }
+    return 0;
+}
